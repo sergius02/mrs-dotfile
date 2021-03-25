ea0b3468244eafbc5eb4e4a5028b5fcd2daa842a
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-03-25 18:17:16 +0100

    🐛 Fix some emojis issues

--- sym.sh
+++ sym.sh
@@ -7,3 +7,7 @@ ln -s $HOME/.dotfiles/shells/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/shells/zsh/.fzf.zsh $HOME/.fzf.zsh
 
 ln -s $HOME/.dotfiles/layouts/keyboard/.dygma_layers.json $HOME/.dygma_layouts.json
+
+# Emojis config
+mkdir $HOME/.config/fontconfig/conf.d
+ln -s $HOME/.dotfiles/fonts/confs/01-emoji.conf $HOME/.config/fontconfig/conf.d
