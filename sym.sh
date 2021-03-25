560fc5d731eefdb658fc31a1876108aaba874bec
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-03-25 18:52:16 +0100

    💄 Added Dracula theme to ZSH

--- sym.sh
+++ sym.sh
@@ -12,4 +12,6 @@ ln -s $HOME/.dotfiles/layouts/keyboard/.dygma_layers.json $HOME/.dygma_layouts.j
 mkdir $HOME/.config/fontconfig/conf.d
 ln -s $HOME/.dotfiles/fonts/confs/01-emoji.conf $HOME/.config/fontconfig/conf.d
 
+# ZSH stuff
 ln -s $HOME/.dotfiles/modules/ohmyzsh $HOME/.oh-my-zsh
+ln -s $HOME/.dotfiles/shells/zsh/themes/dracula-zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
