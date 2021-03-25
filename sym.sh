bdcd23fcf583492eec4e82c284b2c702cd84d198
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-03-25 18:58:19 +0100

    ✨ Added zsh-autosuggestions to ZSH

--- sym.sh
+++ sym.sh
@@ -15,3 +15,4 @@ ln -s $HOME/.dotfiles/fonts/confs/01-emoji.conf $HOME/.config/fontconfig/conf.d
 # ZSH stuff
 ln -s $HOME/.dotfiles/modules/ohmyzsh $HOME/.oh-my-zsh
 ln -s $HOME/.dotfiles/shells/zsh/themes/dracula-zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
+ln -s $HOME/.dotfiles/shells/zsh/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
