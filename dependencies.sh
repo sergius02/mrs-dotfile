cf4e63fd5a0da5f4d6387fc168204b988e9cc4a9
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-03-27 11:33:28 +0100

    ✨ Add JetBrains Toolbox installation

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,6 @@
 # Things i need
 sudo pacman -S jre8-openjdk jdk8-openjdk neovim python-pynvim postgresql pgadmin4 python python-pip wget
+
+mkdir -p $HOME/.jetbrains-toolbox
+wget https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.20.7940.tar.gz -P $HOME/.jetbrains-toolbox
+cd $HOME/.jetbrains-toolbox && tar -xvf jetbrains-toolbox-1.20.7940.tar.gz && ./jetbrains-toolbox
