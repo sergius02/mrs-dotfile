824c4aafb8ea9f710ed94f52c7046cb0ccfd3ec5
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-04-28 16:55:19 +0200

    ✨ Added xranrdr config

---
+++ README.md
@@ -0,0 +1,2 @@
+* Execute xrandr script in lightdm
+display-setup-script=/home/sergio/.screenlayout/screens_layout.sh
---
+++ desktop/configurations/xrandr/screens_layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --mode 1920x1080 --pos 1920x0 --rotate right --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x312 --rotate normal --output VIRTUAL1 --off
