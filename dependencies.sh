1408d6886a858c977b3864aaf4ce92b3912da087
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-07-23 11:22:35 +0200

    :arrow_up: Upgraded JetBrains Toolbox

--- dependencies.sh
+++ dependencies.sh
@@ -2,5 +2,5 @@
 sudo apt install openjdk-8-jre openjdk-8-jdk neovim python-pynvim postgresql python3 python3-pip wget exa
 
 mkdir -p $HOME/.jetbrains-toolbox
-wget https://download-cf.jetbrains.com/toolbox/jetbrains-toolbox-1.20.7940.tar.gz -P $HOME/.jetbrains-toolbox
-cd $HOME/.jetbrains-toolbox && tar -xvf jetbrains-toolbox-1.20.7940.tar.gz && ./jetbrains-toolbox
+wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.21.9547.tar.gz -P $HOME/.jetbrains-toolbox
+cd $HOME/.jetbrains-toolbox && tar -xvf jetbrains-toolbox-1.21.9547.tar.gz && ./jetbrains-toolbox
