3adce06e32f648cbf840e92357a685fc891c0368
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-09-07 10:02:54 +0200

    :coffin: Remove unused symlinks

--- sym.sh
+++ sym.sh
@@ -10,13 +10,6 @@ ln -s $HOME/.dotfiles/shells/zsh/.fzf.zsh $HOME/.fzf.zsh
 ln -s $HOME/.dotfiles/layouts/keyboard/.dygma_layers.json $HOME/.dygma_layouts.json
 ln -s $HOME/.dotfiles/layouts/keyboard/.allMacros.json $HOME/.allMacros.json
 
-# Emojis config
-mkdir $HOME/.config/fontconfig/conf.d
-ln -s $HOME/.dotfiles/fonts/confs/01-emoji.conf $HOME/.config/fontconfig/conf.d
-
-# A nice theme for GTK
-ln -s $HOME/.dotfiles/desktop/themes/dracula-theme $HOME/.themes/dracula-theme
-
 # ZSH stuff
 ln -s $HOME/.dotfiles/modules/ohmyzsh $HOME/.oh-my-zsh
 ln -s $HOME/.dotfiles/shells/zsh/themes/dracula-zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
@@ -27,9 +20,6 @@ ln -s $HOME/.dotfiles/terminals/alacritty $HOME/.config/
 
 # Polybar
 ln -s $HOME/.dotfiles/desktop/applications/polybar $HOME/.config/
-
-# Rofi
-ln -s $HOME/.dotfiles/desktop/applications/rofi $HOME/.config
 
 # IdeaVIM
 ln -s $HOME/.dotfiles/editors/intellij/.ideavimrc $HOME/
