2172f07b4b7cc06511ba888aecadd869496b93fd
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-09-07 10:01:39 +0200

    :hammer: Post install Alacritty configurations

--- post-install.sh
+++ post-install.sh
@@ -3,3 +3,24 @@
 sh modules/fzf/install
 
 regolith-look set dracula
+
+# Alacritty build
+mkdir -p $HOME/Programas
+git clone https://github.com/alacritty/alacritty.git $HOME/Programas/Alacritty
+cd $HOME/Programas/Alacritty
+
+rustup override set stable
+rustup update stable
+
+cargo build --release
+
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+
