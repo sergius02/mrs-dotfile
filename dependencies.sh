ca36e313bf5b1de156abc3717bc37a4d47c0e4de
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-09-07 09:56:51 +0200

    :heavy_plus_sign: Added Brave browser

--- dependencies.sh
+++ dependencies.sh
@@ -4,3 +4,14 @@ sudo apt install git openjdk-8-jre openjdk-8-jdk neovim postgresql python3 pytho
 mkdir -p $HOME/.jetbrains-toolbox
 wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.21.9547.tar.gz -P $HOME/.jetbrains-toolbox
 cd $HOME/.jetbrains-toolbox && tar -xvf jetbrains-toolbox-1.21.9547.tar.gz && ./jetbrains-toolbox
+
+# Brave browser
+sudo apt install apt-transport-https curl
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg https://brave-browser-apt-nightly.s3.brave.com/brave-browser-nightly-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-nightly-archive-keyring.gpg arch=amd64] https://brave-browser-apt-nightly.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-nightly.list
+
+sudo apt update
+
+sudo apt install brave-browser-nightly
