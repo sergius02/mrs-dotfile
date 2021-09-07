de5e447160732dd3e38482b6534057a05ca9ae51
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-09-07 09:56:17 +0200

    :arrow_up: Added a lot of dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,5 @@
 # Things i need
-sudo apt install openjdk-8-jre openjdk-8-jdk neovim python-pynvim postgresql python3 python3-pip wget exa
+sudo apt install git openjdk-8-jre openjdk-8-jdk neovim postgresql python3 python3-pip wget exa telegram-desktop regolith-look-dracula cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 zsh
 
 mkdir -p $HOME/.jetbrains-toolbox
 wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.21.9547.tar.gz -P $HOME/.jetbrains-toolbox
