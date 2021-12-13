001cc29ed13277a0aa238eefdadf2317001e7142
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-12-13 20:06:21 +0100

    🔧 Fixing sym.sh

--- sym.sh
+++ sym.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-mkdir -p $HOME/.npm-packages
+#######################
+# Create the symlinks #
+#######################
+
+# ZSH stuff
 ln -fs $HOME/.dotfiles/shells/zsh/.zshrc $HOME/.zshrc
 ln -fs $HOME/.dotfiles/shells/zsh/.fzf.zsh $HOME/.fzf.zsh
 
 # NPM packages folder to user home
 mkdir -p $HOME/.npm-packages
 ln -fs $HOME/.dotfiles/shells/npm/.npmrc $HOME/.npmrc
-
-# ZSH stuff
-ln -fs $HOME/.dotfiles/shells/zsh/themes/dracula-zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
-ln -fs $HOME/.dotfiles/shells/zsh/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Alacritty
 mkdir -p $HOME/.config/alacritty
@@ -28,10 +28,6 @@ ln -fs $HOME/.dotfiles/modules/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my
 # Regolith
 ln -fs $HOME/.dotfiles/desktop/regolith/i3/config $HOME/.config/regolith/i3/config
 ln -fs $HOME/.dotfiles/desktop/regolith/Xresources $HOME/.config/regolith/Xresources
-
-# Ulauncher
-mkdir -p $HOME/.config/ulauncher/user-themes
-ln -fs $HOME/.dotfiles/desktop/applications/ulauncher/dracula-theme $HOME/.config/ulauncher/user-themes/dracula-ulauncher
 
 # Polybar
 ln -fs $HOME/.dotfiles/desktop/applications/polybar-config/config.ini $HOME/.config/polybar/panels
