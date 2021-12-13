3d65ba7e5a1912d5907ab1ffdccbd996657ace92
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-12-13 11:57:33 +0100

    🔧 Changes in symlinks
    * Remove the links for Spacevim, fzf and oh-my-zsh
    * Force symlinks

--- sym.sh
+++ sym.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-ln -s $HOME/.dotfiles/modules/SpaceVim $HOME/.SpaceVim
-ln -s $HOME/.dotfiles/modules/fzf $HOME/.fzf
-
-ln -s $HOME/.dotfiles/shells/zsh/.zshrc $HOME/.zshrc
-ln -s $HOME/.dotfiles/shells/zsh/.fzf.zsh $HOME/.fzf.zsh
+ln -fs $HOME/.dotfiles/shells/zsh/.zshrc $HOME/.zshrc
+ln -fs $HOME/.dotfiles/shells/zsh/.fzf.zsh $HOME/.fzf.zsh
 
 # ZSH stuff
-ln -s $HOME/.dotfiles/modules/ohmyzsh $HOME/.oh-my-zsh
-ln -s $HOME/.dotfiles/shells/zsh/themes/dracula-zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
-ln -s $HOME/.dotfiles/shells/zsh/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -fs $HOME/.dotfiles/shells/zsh/themes/dracula-zsh/dracula.zsh-theme $HOME/.oh-my-zsh/themes/dracula.zsh-theme
+ln -fs $HOME/.dotfiles/shells/zsh/plugins/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 # Alacritty
-ln -s $HOME/.dotfiles/terminals/alacritty $HOME/.config/
+ln -fs $HOME/.dotfiles/terminals/alacritty $HOME/.config/
 
 # Polybar
-ln -s $HOME/.dotfiles/desktop/applications/polybar $HOME/.config/
+ln -fs $HOME/.dotfiles/desktop/applications/polybar $HOME/.config/
 
 # IdeaVIM
-ln -s $HOME/.dotfiles/editors/intellij/.ideavimrc $HOME/
+ln -fs $HOME/.dotfiles/editors/intellij/.ideavimrc $HOME/
 
 # Spaceship prompt for ZSH
-ln -s $HOME/.dotfiles/modules/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/custom/themes
+ln -fs $HOME/.dotfiles/modules/spaceship-prompt/spaceship.zsh-theme $HOME/.oh-my-zsh/custom/themes
 
 # Regolith
-ln -s $HOME/.dotfiles/desktop/regolith/i3 $HOME/.config/regolith/i3
-ln -s $HOME/.dotfiles/desktop/regolith/Xresources $HOME/.config/regolith/Xresources
+ln -fs $HOME/.dotfiles/desktop/regolith/i3/config $HOME/.config/regolith/i3/config
+ln -fs $HOME/.dotfiles/desktop/regolith/Xresources $HOME/.config/regolith/Xresources
 
 # Ulauncher
 mkdir -p $HOME/.config/ulauncher/user-themes
-ln -s $HOME/.dotfiles/desktop/applications/ulauncher/dracula-theme $HOME/.config/ulauncher/user-themes/dracula-ulauncher
+ln -fs $HOME/.dotfiles/desktop/applications/ulauncher/dracula-theme $HOME/.config/ulauncher/user-themes/dracula-ulauncher
 
 # Polybar
 ln -fs $HOME/.dotfiles/desktop/applications/polybar-config/config.ini $HOME/.config/polybar/panels
@@ -37,6 +33,7 @@ ln -fs $HOME/.dotfiles/desktop/applications/polybar-config/launch.sh $HOME/.conf
 
 # Git config
 ln -fs $HOME/.dotfiles/desktop/git/.gitconfig_global $HOME/.gitconfig
+mkdir -p $HOME/Arvo
 ln -fs $HOME/.dotfiles/desktop/git/.gitconfig_arvo $HOME/Arvo/.gitconfig
 
 # Regolith background
