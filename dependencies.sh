e4b130c605a4f64595fddd61c8ce3cf733342c0c
Sergio Fernández Celorio <scelorio1990@gmail.com>
2021-12-16 14:24:53 +0100

    ➕ Gnome Boxes and Remmina

--- dependencies.sh
+++ dependencies.sh
@@ -3,7 +3,7 @@
 ############################
 
 # Things i need
-sudo apt install -y git openjdk-8-jre openjdk-8-jdk neovim postgresql python3 python3-pip wget exa telegram-desktop regolith-look-dracula cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 zsh
+sudo apt install -y git openjdk-8-jre openjdk-8-jdk neovim postgresql python3 python3-pip wget exa telegram-desktop regolith-look-dracula cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 zsh gnome-boxes remmina remmina-plugin-rdp
 
 mkdir -p $HOME/.jetbrains-toolbox
 wget https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-1.21.9547.tar.gz -P $HOME/.jetbrains-toolbox
