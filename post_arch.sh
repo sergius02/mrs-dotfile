816187c64eef533fac69706453e1bd89192bc6d8
Sergio Fernández Celorio <scelorio1990@gmail.com>
2022-02-03 13:45:47 +0100

    🐛 Vim plug installation missed

--- post_arch.sh
+++ post_arch.sh
@@ -12,3 +12,7 @@ npm install -g pyright yarn pm2 vue@next @angular/cli typescript typescript-lang
 sudo passwd postgres
 su - postgres -c "initdb --locale en_US.UTF-8 -D '/var/lib/postgres/data'"
 su - postgres -c "psql -c \"CREATE ROLE sergio SUPERUSER LOGIN PASSWORD 'postgres'\""
+
+# Vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
